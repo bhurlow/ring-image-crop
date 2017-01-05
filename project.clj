927f55f87017240f2c1e1c72c28e826b02fbad4f
Brian Hurlow <brian@brianhurlow.com>
2017-01-04 23:10:20 -0500

    release

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :main ring-image-crop.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]]
   :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]
                                   [ring "1.4.0"]]}})
